5ad71a95ab2f017f4ac58c4ec341d62d968d60d8
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-10-07 13:54:21 +0700

    upload prak2

--- minggu06_jobsheet06/layout_flutter/lib/main.dart
+++ minggu06_jobsheet06/layout_flutter/lib/main.dart
@@ -6,6 +6,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   
   @override
+
+  // == PRAKTIKUM 1 ==
   Widget build(BuildContext context) {
     Widget titleSection = Container(
       padding: const EdgeInsets.all(16.0),
@@ -43,6 +45,17 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Rizkya Salsabila_2341720056',
       home: Scaffold(
@@ -52,9 +65,32 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection,
           ],
         ),
       ),
+    );
+  }
+
+  // == PRAKTIKUM 2 ==
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
